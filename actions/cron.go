fe30eb362394dc98b68da4478609ef092b3934f8
Gustavo Moya <gmoyatorres@gmail.com>
2020-10-31 22:34:03 -0400

    Modified the cron function so that instead of using the cron package I use goroutines and a ticker.

--- actions/cron.go
+++ actions/cron.go
@@ -19,65 +19,61 @@ const schedule string = "@every 12h"
 // for new chapters. If a new chapter is found, a message
 // is sent to the Chat that got subscribed to the title.
 func GetMangaUpdates(job *models.Job, bot *tb.Bot) {
-	job.Cron.AddFunc(schedule, func() {
-		jobName := "GetMangaUpdates"
-		log.Println("Running Manga Updates CRON")
-		started := time.Now()
-		fmt.Println("*** [*] CRON job 'GetMangaUpdates' started ***")
-		fmt.Printf("*** [*] CRON job 'GetMangaUpdates' start time: %v ***\n", started)
+	jobName := "GetMangaUpdates"
 
-		cursor, err := job.DB.MongoClient.Collection("subscription").Find(job.DB.Ctx, bson.M{})
-		if err != nil {
-			onError(jobName, started, err)
-		}
-
-		subs := make([]*models.Subscription, 0)
-
-		err = cursor.All(job.DB.Ctx, &subs)
-		if err != nil {
-			onError(jobName, started, err)
-		}
-
-		for _, manga := range subs {
-			feed := NewMangaInterface(manga.MangaFeed, job.DB)
-			if feed == nil {
-				continue
+	for t := range time.NewTicker(time.Hour * 6).C {
+		log.Println("Running Manga Updates Goroutine...", t)
+		go func() {
+			started := time.Now()
+			cursor, err := job.DB.MongoClient.Collection("subscription").Find(job.DB.Ctx, bson.M{})
+			if err != nil {
+				onError(jobName, started, err)
 			}
 
-			// Get the last chapter for each manga
-			last, err := feed.GetLastMangaChapter(manga.MangaURL)
-			if err != nil || last == "" {
-				continue // LAter will decide what to do here
+			subs := make([]*models.Subscription, 0)
+
+			err = cursor.All(job.DB.Ctx, &subs)
+			if err != nil {
+				onError(jobName, started, err)
 			}
 
-			if manga.LastChapterURL == "" || last != manga.LastChapterURL {
-				manga.LastChapterURL = last
-				msg := fmt.Sprintf("Here is a new chapter for %s\n %s", manga.MangaName, last)
-				to, _ := bot.ChatByID(strconv.FormatInt(manga.ChatID, 10))
-				bot.Send(to, msg)
-				updateLastChapter(manga, job)
+			for _, manga := range subs {
+				feed := NewMangaInterface(manga.MangaFeed, job.DB)
+				if feed == nil {
+					continue
+				}
+
+				// Get the last chapter for each manga
+				last, err := feed.GetLastMangaChapter(manga.MangaURL)
+				if err != nil || last == "" {
+					continue // LAter will decide what to do here
+				}
+
+				if manga.LastChapterURL == "" || last != manga.LastChapterURL {
+					manga.LastChapterURL = last
+					msg := fmt.Sprintf("Here is a new chapter for %s\n %s", manga.MangaName, last)
+					to, _ := bot.ChatByID(strconv.FormatInt(manga.ChatID, 10))
+					bot.Send(to, msg)
+					updateLastChapter(manga, job)
+				}
 			}
-		}
-
-		onSuccess(jobName, started)
-	})
-
-	job.Cron.Start()
+		}()
+	}
 }
 
 func onError(name string, started time.Time, err error) {
 	ended := time.Now()
-	fmt.Printf("*** [*] CRON job '%s' finished unexpectedly ***", name)
-	fmt.Printf("*** [*] CRON job '%s' Errors: [%v] ***\n", name, err)
-	fmt.Printf("*** [*] CRON job '%s' end time: %v ***\n", name, ended)
-	fmt.Printf("*** [*] CRON job '%s' time elapsed: %v ***\n", name, ended.Sub(started))
+	fmt.Printf("*** [*] Goroutine '%s' finished unexpectedly ***", name)
+	fmt.Printf("*** [*] Goroutine '%s' Errors: [%v] ***\n", name, err)
+	fmt.Printf("*** [*] Goroutine '%s' end time: %v ***\n", name, ended)
+	fmt.Printf("*** [*] Goroutine '%s' time elapsed: %v ***\n", name, ended.Sub(started))
 }
 
 func onSuccess(name string, started time.Time) {
 	ended := time.Now()
-	fmt.Printf("*** [*] CRON job '%s' finished succesfully ***", name)
-	fmt.Printf("*** [*] CRON job '%s' end time: %v ***\n", name, ended)
-	fmt.Printf("*** [*] CRON job '%s' time elapsed: %v ***\n", name, ended.Sub(started))
+	fmt.Printf("*** [*] Goroutine '%s' finished succesfully ***", name)
+	fmt.Printf("*** [*] Goroutine '%s' end time: %v ***\n", name, ended)
+	fmt.Printf("*** [*] Goroutine '%s' time elapsed: %v ***\n", name, ended.Sub(started))
 }
 
 func updateLastChapter(manga *models.Subscription, job *models.Job) {
