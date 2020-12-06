34881f82c7ae0cc36e857338b8a6ac279a63b824
Gustavo Moya <gmoyatorres@gmail.com>
2020-12-06 19:03:34 -0400

    Removing cron package dependency.

--- models/job.go
+++ models/job.go
@@ -1,11 +1,8 @@
 package models
 
-import "github.com/robfig/cron"
-
 // Job is a struct that
 // contains some properties that
 // are used inside the CRON jobs
 type Job struct {
-	Cron *cron.Cron
-	DB   *DatabaseConfig
+	DB *DatabaseConfig
 }
